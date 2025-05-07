5c8d35c68dd98b0da69b9a58d9126fb234973e20
MoeMyintThaw014 <moethaw019@gmail.com>
2025-05-07 14:16:10 +0630

    commit read db

---
+++ src/main/java/org/DevOps/Database_read.java
@@ -0,0 +1,31 @@
+package org.DevOps;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Database_read {
+    private Connection con;
+
+    public Database_read(Connection con) {
+        this.con = con;
+    }
+
+    protected void read(){
+        try {
+            PreparedStatement ps = con.prepareStatement(
+                  "SELECT * FROM offices WHERE country =?");
+            ps.setString(1,"USA");
+            ResultSet rs=ps.executeQuery();
+            while (rs.next()){
+                System.out.print(rs.getInt(1));
+                System.out.println("|");
+                System.out.println(rs.getString("city"));
+                System.out.println();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
