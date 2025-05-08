566aebcf2b0f123a8d636ba4f3e80c484e6bc397
MoeMyintThaw014 <moethaw019@gmail.com>
2025-05-08 13:07:03 +0630

    commit read db

--- src/main/java/org/DevOps/Database_read.java
+++ src/main/java/org/DevOps/Database_read.java
@@ -6,24 +6,22 @@ import java.sql.ResultSet;
 
 public class Database_read {
     private Connection con;
-
     public Database_read(Connection con) {
         this.con = con;
     }
-
     protected void read(){
         try {
-            PreparedStatement ps = con.prepareStatement(
-                  "SELECT * FROM offices WHERE country =?");
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM offices WHERE country =?");
             ps.setString(1,"USA");
             ResultSet rs=ps.executeQuery();
             while (rs.next()){
                 System.out.print(rs.getInt(1));
-                System.out.println("|");
-                System.out.println(rs.getString("city"));
+                System.out.print("|");
+                System.out.print(rs.getString("city"));
                 System.out.println();
             }
-
+            rs.close();
+            ps.close();
         }catch (Exception e){
             e.printStackTrace();
         }
